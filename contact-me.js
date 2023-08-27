e9af282b5c50e9ea828386eb58159b909dcf806f
daynawhite <dayna26.2@gmail.com>
2023-08-27 15:03:30 -0500

    contact form and results page added

---
+++ contact-me.js
@@ -0,0 +1,40 @@
+
+// const placeForResults = document.getElementById("results")
+
+// new URLSearchParams(window.location.search).forEach((value, name) => {
+//   placeForResults.append(`${name} : ${value}`)
+//   placeForResults.append(document.createElement("br"))
+// })
+console.log('hello world')
+
+const form = document.querySelector('.contact-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const comments = document.getElementById('comments');
+const message = document.querySelector('.message');
+// console.log(form)
+// console.log(nameInput.value)
+// console.log(emailInput)
+// console.log(comments)
+let username;
+let email;
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    username = nameInput.value;
+    email = emailInput.value;
+
+    const p = document.createElement('p');
+    p.textContent = `Hi ${username} welcome to the site! You've signed in as ${email}`;
+
+    message.appendChild(p);
+})
+
+nameInput.addEventListener('input',function(event) {
+    username = event.target.value;
+})
+console.log(username)
+
+emailInput.addEventListener('input', function(event){
+})
